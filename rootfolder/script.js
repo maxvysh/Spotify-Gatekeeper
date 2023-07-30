c6857e229eaa3bfd8c8e97f44f8a7db01b326ff2
maxvysh <45989000+maxvysh@users.noreply.github.com>
2023-07-30 01:05:27 -0400

    1

---
+++ rootfolder/script.js
@@ -0,0 +1,26 @@
+function loginWithSpotify() {
+    const clientId = '89fb532c08584190b4d8d6e99b5b0e23';
+    const redirectUri = 'http://127.0.0.1:5500/rootfolder/signedin.html';
+    const scopes = ['user-read-private', 'user-read-email', 'user-top-read']; // Add the necessary scopes for your app
+
+    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
+
+    window.location.href = authUrl;
+}
+
+var stupidn = "dumbfuck";
+var accessToken = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+        if (item) {
+            var parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+        }
+        return initial;
+    }, {}).access_token;
+
+  // Function to display the accessToken on the page
+  const accessTokenContainer = document.getElementById('access-token-container');
+  accessTokenContainer.textContent = `Access Token: ${accessToken}`;
+  
---
+++ rootfolder/script2.js
@@ -0,0 +1,2 @@
+const accessTokenContainer2 = document.getElementById('access-token-container2');
+accessTokenContainer2.textContent = `Access Token: ${accessToken}`;
